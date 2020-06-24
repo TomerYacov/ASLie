5eafa03af52bbdeffe022d953c5370e2c09db87b
Amit Neeman <amitneeman01@gmail.com>
2020-06-24 19:29:01 +0300

    with real mapping

--- predictor/hModel/hierarchical_model.py
+++ predictor/hModel/hierarchical_model.py
@@ -1,6 +1,9 @@
 from os import listdir
+
 import numpy as np
 from tensorflow.python.keras.saving.save import load_model
+
+
 
 
 class HierarchicalModel:
@@ -16,6 +19,19 @@ class HierarchicalModel:
             self.models[index] = load_model(f"{basedir}/{d}")
             print(f"Loaded {d} model")
 
+        self.clusters = [
+            ["Y", "space", "nothing", "X"],
+            ["A", "E", "M", "N", "T", "S", "I", "J"],
+            ["B", "D", "F", "Z", "L"],
+            ["U", "V", "W", "K", "R"],
+            ["G", "H", "P", "Q", "del"],
+            ["O", "C"],
+        ]
+        self.mapping = {"0": "A", "1": "B", "2": "C", "3": "D", "4": "E", "5": "F", "6": "G", "7": "H", "8": "I",
+                        "9": "J", "10": "K", "11": "L", "12": "M", "13": "N", "14": "O", "15": "P", "16": "Q",
+                        "17": "R", "18": "S", "19": "T", "20": "U", "21": "V", "22": "W", "23": "X", "24": "Y",
+                        "25": "Z", "26": "del", "27": "nothing", "28": "space"}
+
     def predict(self, X):
         cp = self.high_model(X)
         p = np.zeros((X.shape[0], self.num_classes))
@@ -26,3 +42,5 @@ class HierarchicalModel:
 
     def __call__(self, X):
         return self.predict(X)
+
+
